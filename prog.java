3165ca5e09a8352e0c1a0ef7c452116fe4665b5f
Marek Rychlik <rychlik@arizona.edu>
2023-03-09 10:38:37 -0700

    *** empty log message ***

---
+++ prog.java
@@ -0,0 +1,32 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.apache.pdfbox.io.RandomAccessFile;
+import org.apache.pdfbox.pdfparser.PDFParser;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+
+public class prog {
+    String tablePDF = "samples/bus.pdf";
+
+    public static void main(String args[]) {
+	this.parsePDF(tablePDF);
+    }
+
+    protected void parsePDF(String filename) {
+	String string = null;
+	try {
+	    PDFParser pdfParser = new PDFParser(new RandomAccessFile(new File(filename), "r"));
+	    pdfParser.parse();
+	    PDDocument pdDocument = new PDDocument(pdfParser.getDocument());
+	    PDFTextStripper pdfTextStripper = new PDFLayoutTextStripper();
+	    string = pdfTextStripper.getText(pdDocument);
+	} catch (FileNotFoundException e) {
+	    e.printStackTrace();
+	} catch (IOException e) {
+	    e.printStackTrace();
+	}
+	System.out.println(string);
+    }
+}
