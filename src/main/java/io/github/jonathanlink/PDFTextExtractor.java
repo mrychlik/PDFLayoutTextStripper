ffea6f5200dd027281e3381f5aee72812930fa63
Marek Rychlik <rychlik@arizona.edu>
2023-03-22 15:18:40 -0700

    *** empty log message ***

--- src/main/java/io/github/jonathanlink/PDFTextExtractor.java
+++ src/main/java/io/github/jonathanlink/PDFTextExtractor.java
@@ -8,16 +8,23 @@ import org.apache.pdfbox.io.RandomAccessFile;
 import org.apache.pdfbox.pdfparser.PDFParser;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.text.PDFTextStripper;
+import java.util.*;
 
 class PDFTextExtractor {
-    public static java.lang.String parsePDF(String filename) {
+    public static List<String> parsePDF(String filename) {
+	ArrayList<String> pages = new ArrayList<String>();
 	try { 
 	    RandomAccessFile file = new RandomAccessFile(new File(filename), "r");
 	    PDFParser pdfParser = new PDFParser(file);
 	    pdfParser.parse();
 	    PDDocument pdDocument = pdfParser.getPDDocument();
 	    PDFTextStripper pdfTextStripper = new PDFLayoutTextStripper();
-	    String string = pdfTextStripper.getText(pdDocument);
+	    for (int p = 1; p < pdDocument.getNumberOfPages(); p++) {
+		pdfTextStripper.setStartPage(p);
+		pdfTextStripper.setEndPage(p + 1);
+		String pageOfText = pdfTextStripper.getText(pdDocument);
+		pages.add(pageOfText);
+	    }
 	    pdDocument.close();
 	    return string;
 	} catch (FileNotFoundException e) {
