d3cb3434b54c4eca72a763edcf51014128d54311
Marek Rychlik <rychlik@arizona.edu>
2023-03-22 13:06:12 -0700

    *** empty log message ***

--- src/main/java/io/github/jonathanlink/PDFLayoutTextStripperExe.java
+++ src/main/java/io/github/jonathanlink/PDFLayoutTextStripperExe.java
@@ -1,14 +1,5 @@
 package io.github.jonathanlink;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-
-import org.apache.pdfbox.io.RandomAccessFile;
-import org.apache.pdfbox.pdfparser.PDFParser;
-import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.text.PDFTextStripper;
-
 public class PDFLayoutTextStripperExe {
 
     /**
@@ -19,21 +10,5 @@ public class PDFLayoutTextStripperExe {
 		PDFTextExtractor pdfTextExtractor = new PDFTextExtractor();
 		String string = pdfTextExtractor.parsePDF(tablePDF);
 		System.out.println(string);
-    }
-
-    protected void parsePDF(String filename) {
-	String string = null;
-	try {
-	    PDFParser pdfParser = new PDFParser(new RandomAccessFile(new File(filename), "r"));
-	    pdfParser.parse();
-	    PDDocument pdDocument = new PDDocument(pdfParser.getDocument());
-	    PDFTextStripper pdfTextStripper = new PDFLayoutTextStripper();
-	    string = pdfTextStripper.getText(pdDocument);
-	} catch (FileNotFoundException e) {
-	    e.printStackTrace();
-	} catch (IOException e) {
-	    e.printStackTrace();
-	}
-
     }
 }
