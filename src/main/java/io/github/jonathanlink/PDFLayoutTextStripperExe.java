835070bd77a7a10a47af40582e0c41005b76f9ba
Marek Rychlik <rychlik@arizona.edu>
2023-03-22 15:37:50 -0700

    *** empty log message ***

--- src/main/java/io/github/jonathanlink/PDFLayoutTextStripperExe.java
+++ src/main/java/io/github/jonathanlink/PDFLayoutTextStripperExe.java
@@ -8,8 +8,7 @@ public class PDFLayoutTextStripperExe {
      */
     public static void main(String[] args) {
 	final String tablePDF = "samples/bus.pdf";
-	PDFTextExtractor pdfTextExtractor = new PDFTextExtractor();
-	List<String> pages = pdfTextExtractor.parsePDF(tablePDF);
+	List<String> pages = PDFTextExtractor.parsePDF(tablePDF);
 	for (int p = 0; p < pages.size(); p++) {
 	    System.out.println(pages.get(p));
 	}
